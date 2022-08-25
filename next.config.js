7c282eec6659e61cad2dedc8df1461ab89a26e79
j.zaripov <zaripovjasurbek2001@gmail.com>
2022-08-25 12:35:52 +0500

    added next-translate

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
+const nextTranslate = require('next-translate');
+
+/** @type {import('next').NextConfig}*/
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  ...nextTranslate(),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
